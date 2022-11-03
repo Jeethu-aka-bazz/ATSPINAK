8c1cd4fb6d87dacf6269f71357de673ff22f98a0
Jegajeeth <jegajeeth.manikandan@increscotech.com>
2022-11-03 16:23:13 +0530

    feat(web): react native web completed

--- index.web.js
+++ index.web.js
@@ -2,3 +2,8 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 
 AppRegistry.registerComponent('ATSPinak', () => App);
+
+AppRegistry.runApplication('ATSPinak', {
+  initialProps: {},
+  rootTag: document.getElementById('app-root'),
+});
